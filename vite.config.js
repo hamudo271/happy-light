51ac3b745824a17e3f791935cba673f4493db5bb
조현도 <mcthemax@emssideomaegseuui-MacBookPro.local>
2026-02-15 14:41:33 +0900

    fix: set router basename and refine cross-platform base path

--- vite.config.js
+++ vite.config.js
@@ -23,10 +23,10 @@
 
 // Vite 설정 함수
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const isGHDeplpoy = process.env.NODE_ENV === 'production' && !process.env.RAILWAY_ENVIRONMENT && !process.env.PORT;
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' && !process.env.RAILWAY_ENVIRONMENT ? '/happy-light/' : '/',
+  base: isGHDeplpoy ? '/happy-light/' : '/',
 
   plugins: [
     react()
